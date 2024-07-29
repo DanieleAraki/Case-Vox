77cec19e82e584f2ab416aad7e4fb78bef8f8c60
DanieleAraki <88806573+DanieleAraki@users.noreply.github.com>
2024-07-28 22:47:12 -0300

    Update dani_login.cy.js

--- cypress/e2e/login/dani_login.cy.js
+++ cypress/e2e/login/dani_login.cy.js
@@ -9,7 +9,6 @@ describe('Teste Funcional de Login', () => {
         cy.get('[data-test="login-button"]').click()
         cy.get('[data-test="title"]').should('contain','Products')
     });
-});
 it('Validação Login Incorreto', () => {
     cy.visit("https://www.saucedemo.com/");
     cy.get('[data-test="username"]').type("standard_userL")
@@ -27,6 +26,6 @@ it('Validação Senha Incorreta', () => {
     cy.get('[data-test="password"]').type("secret_sauce5")
     cy.get('[data-test="login-button"]').click()
     cy.get('[data-test="error"]').should('contain', 'Epic sadface: Username and password do not match any user in this service')
-
     
+    });    
 });
